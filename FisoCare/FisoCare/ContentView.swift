0ec3d144c8e89eb8ab5003c4ca80d594837c1c2a
XimeBellakitta <318550574@pcpuma.acatlan.unam.mx>
2023-10-18 09:16:38 -0600

    FisoCare
    quiero conectar firebase a esta app

---
+++ FisoCare/FisoCare/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  FisoCare
+//
+//  Created by ximena juana mejia jacobo on 17/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                   
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FisoCare/FisoCare/FisoCareApp.swift
@@ -0,0 +1,22 @@
+//
+//  FisoCareApp.swift
+//  FisoCare
+//
+//  Created by ximena juana mejia jacobo on 17/10/23.
+//
+
+import SwiftUI
+
+
+
+
+
+
+@main
+struct FisoCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
